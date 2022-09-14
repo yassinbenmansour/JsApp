c9e1cec9c57217375aa5a88bf78226ac804742de
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-09-14 22:45:14 +0100

    open close win

---
+++ close-window/script.js
@@ -0,0 +1,7 @@
+var win; 
+      function openW() { 
+        win = window.open("https://github.com/yassinbenmansour", "_blank", "width=900, height=786"); 
+      }
+      function closeW() { 
+        win.close(); 
+      }
