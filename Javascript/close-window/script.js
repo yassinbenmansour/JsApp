2a6dfae4a07ae0f355ef2bac50c12e8fb30edce5
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-09-28 00:04:31 +0100

    update

--- Javascript/close-window/script.js
+++
@@ -1,7 +0,0 @@
-var win; 
-      function openW() { 
-        win = window.open("https://github.com/yassinbenmansour", "_blank", "width=900, height=786"); 
-      }
-      function closeW() { 
-        win.close(); 
-      }
--- Javascript/rtn/script.js
+++
@@ -1,4 +0,0 @@
- 
-    function rtn() {
-       window.history.back();
-    }
