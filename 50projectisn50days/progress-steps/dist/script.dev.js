b147efec736d9e97100c2d8100e91ce966bdbd2f
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-10-03 19:15:56 +0100

    update

--- 50projectisn50days/progress-steps/dist/script.dev.js
+++ 50projectisn50days/progress-steps/dist/script.dev.js
@@ -1,1 +1,46 @@
 "use strict";
+
+var progress = document.getElementById('progress');
+var prev = document.getElementById('prev');
+var next = document.getElementById('next');
+var circles = document.querySelectorAll('.circle');
+var currentActive = 1;
+next.addEventListener('click', function () {
+  currentActive++;
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+
+  update();
+});
+prev.addEventListener('click', function () {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach(function (circle, idx) {
+    if (idx < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+  var actives = document.querySelectorAll('.active');
+  progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
