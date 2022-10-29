a32eeacbdec1af0f43692666187402997c3d8b0f
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-10-29 20:07:36 +0100

    update

--- app-convert-base/dist/script.dev.js
+++ app-convert-base/dist/script.dev.js
@@ -5,11 +5,8 @@ function convert() {
   var result;
   result = Number(nbrCv).toString(2);
   document.getElementById("result").innerHTML = "Binary (base 2):" + result;
-}
-
-function confirmer() {
-  var res = confirm("Êtes-vous sûr de vouloir supprimer?");
-
-  if (res) {//..................;LOGIQUE
-  }
+  result = Number(nbrCv).toString(8);
+  document.getElementById("result1").innerHTML = "octal (base 8):" + result;
+  result = Number(nbrCv).toString(16);
+  document.getElementById("result2").innerHTML = "hexadecimal (base 16):" + result;
 }
--- app-convert-base/script.js
+++ app-convert-base/script.js
@@ -3,19 +3,15 @@ function convert(){
     let result;
 
     result = Number(nbrCv).toString(2);
-
-
     document.getElementById("result").innerHTML="Binary (base 2):" + result;
+
+    result = Number(nbrCv).toString(8);
+    document.getElementById("result1").innerHTML="octal (base 8):" + result;
+
+    result = Number(nbrCv).toString(16);
+    document.getElementById("result2").innerHTML="hexadecimal (base 16):" + result;
+
+
+
 }
 
-
-
-
-
-
-function confirmer(){
-    var res = confirm("Êtes-vous sûr de vouloir supprimer?");
-    if(res){
-        //..................;LOGIQUE
-    }
-}
