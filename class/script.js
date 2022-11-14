ed8c35b52246ff7edaf9ebbd8c11af6420c7b4a8
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-11-14 23:18:24 +0100

    update

---
+++ class/script.js
@@ -0,0 +1,9 @@
+class Car {
+    constructor(name){
+        this.brand = name;
+    }
+}
+
+const mycar = new Car("Ford");
+
+document.write(mycar.brand);
---
+++ convertMNtoSEC/script.js
@@ -0,0 +1,8 @@
+function ConvertMintoSec(nbr){
+    nbr = nbr * 60;
+    document.getElementById("res").innerHTML= nbr;
+}
+
+
+
+console.log(document.URL);
